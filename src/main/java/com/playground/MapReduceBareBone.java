bc0d0443ff7aa4b3c7d33abc345106e0a49241c4
Guruprasad Bobbi <guruprasad.bobbi@capitalone.com>
2015-04-06 14:52:37 -0400

    typo

--- src/main/java/com/playground/MapReduceBareBone.java
+++ src/main/java/com/playground/MapReduceBareBone.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /*
  * this program is to illustrate what happens when we run a job with no custom mapper, reducer and configuration parameters set
- * executed using >> hadoop jar pro-hadoop.jar org.myorg.defaultsettings.MapReduceBareBone /datasets/wordcount.txt /output/mapreducebarebone
+ * executed using >> hadoop jar pro-hadoop.jar com.playground.MapReduceBareBone /datasets/wordcount.txt /output/mapreducebarebone
  * the framework invokes IdentityMapper which is the default mapper that gets called and writes the 
  * LonWritale byte offset as key and the line intepreted in Text format as value to output file
  * Refer the output from reducer to analyze in detail..
